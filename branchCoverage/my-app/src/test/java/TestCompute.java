61187639393e32067d7307b0e6ad88c9080c004c
kursadsaka <ksaka@etu.edu.tr>
2019-03-01 19:51:05 +0300

    Added a test to test countNumberOfOccurrences with string 'aba' and character 'a'

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -17,24 +17,39 @@ public class TestCompute {
 	}
 
 	@Test
-	public void sizeIs0() {
+	public void sizeIsZero() {
 	  
 		MessageQueue mq = mock(MessageQueue.class);
 		when(mq.size()).thenReturn(0);
 		c = new Compute(mq);
-		assertEquals(-1, c.countNumberOfOccurrences("aa"));
+		assertEquals(-1, c.countNumberOfOccurrences("a"));
 	  
 	}
 	
 	@Test
-	public void sizeIsNot0() {
+	public void sizeIsNotZeroAndContainsFalse() {
 	  
 		MessageQueue mq = mock(MessageQueue.class);
-		String s = "abc";
+		String s = "a";
 		when(mq.size()).thenReturn(12);
 		when(mq.contains(s)).thenReturn(false);
 		c = new Compute(mq);
 		assertEquals(0, c.countNumberOfOccurrences(s));
+	  
+	}
+	
+	@Test
+	public void sizeIsNotZeroAndContainsTrue() {
+	  
+		MessageQueue mq = mock(MessageQueue.class);
+		String s = "a";
+		when(mq.size()).thenReturn(3);
+		when(mq.contains(s)).thenReturn(true);
+		when(mq.getAt(0)).thenReturn("a");
+		when(mq.getAt(1)).thenReturn("b");
+		when(mq.getAt(2)).thenReturn("a");
+		c = new Compute(mq);
+		assertEquals(2, c.countNumberOfOccurrences(s));
 	  
 	}
   
