739c231d1cddd42ee418bd753baf5eb9df256237
kursadsaka <ksaka@etu.edu.tr>
2019-03-01 19:39:20 +0300

    Added 2 tests to test MessageQueue's is 0 and is not

--- branchCoverage/my-app/src/test/java/TestCompute.java
+++ branchCoverage/my-app/src/test/java/TestCompute.java
@@ -6,12 +6,36 @@ import static org.mockito.Mockito.*;
 import org.junit.Test;
 
 public class TestCompute {
-  Compute c;
+  
+	Compute c;
+	
+	@Test
+	public void example() {
+		MessageQueue mq = mock(MessageQueue.class);
+		c = new Compute(mq);
+		assertTrue(true);
+	}
 
-  @Test
-  public void example() {
-    MessageQueue mq = mock(MessageQueue.class);
-    c = new Compute(mq);
-    assertTrue(true);
-  }
+	@Test
+	public void sizeIs0() {
+	  
+		MessageQueue mq = mock(MessageQueue.class);
+		when(mq.size()).thenReturn(0);
+		c = new Compute(mq);
+		assertEquals(-1, c.countNumberOfOccurrences("aa"));
+	  
+	}
+	
+	@Test
+	public void sizeIsNot0() {
+	  
+		MessageQueue mq = mock(MessageQueue.class);
+		String s = "abc";
+		when(mq.size()).thenReturn(12);
+		when(mq.contains(s)).thenReturn(false);
+		c = new Compute(mq);
+		assertEquals(0, c.countNumberOfOccurrences(s));
+	  
+	}
+  
 }
