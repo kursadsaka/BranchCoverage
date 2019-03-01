d2336f942a9e8b761aea0fb9b341db4328b77d18
kursadsaka <ksaka@etu.edu.tr>
2019-03-01 20:09:03 +0300

    Added a test to test Util class when args length is 1

--- branchCoverage/my-app/src/test/java/TestUtil.java
+++ branchCoverage/my-app/src/test/java/TestUtil.java
@@ -6,11 +6,19 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestUtil {
-  Util c;
+	Util c;
 
-  @Before
-  public void setUp() { c = new Util(); }
+	@Before
+	public void setUp() { c = new Util(); }
 
-  @Test
-  public void example() { assertTrue(true); }
+	@Test
+	public void example() { assertTrue(true); }
+	
+	@Test
+	public void lengthIsOne() {
+		
+		assertFalse(c.compute(1));
+		
+	}
+	
 }
